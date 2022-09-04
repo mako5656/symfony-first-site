9fcc8e962a69989e6c8e76eee5c30eee84bf650a
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-05 08:32:16 +0900

    ✨ リダイレクトとRedirectResponseの利用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -12,13 +13,20 @@ class HelloController extends AbstractController
     #[Route('/hello', name:'hello')]
     public function index(Request $request)
     {
-        $name = $request->get('name');
-        $pass = $request->get('pass');
         $result = '<html><body><ol>';
         $result .= '<h1>Parameter</h1>';
-        $result .= '<p>name: ' . $name . '</p>';
-        $result .= '<p>pass: ' . $pass . '</p>';
+        $result .= '<p>This is index page.</p>';
         $result .= '</body></html>';
         return new Response($result);
+    }
+
+    #[Route('/other/{domain}', name:'other')]
+    public function other(Request $request, $domain='')
+    {
+        if ($domain == ''){
+            return $this->redirect('/hello');
+        } else {
+            return new RedirectResponse("http://{$domain}.com");
+        }
     }
 }
