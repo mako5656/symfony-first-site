53945ab3f6cad547c5cfad9bb66a3d0c83e96c89
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-04 21:53:58 +0900

    パラメーターを利用する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,17 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/hello', name:'hello')]
-    public function index()
+    #[Route('/hello/{name}/{pass}', name:'hello')]
+    public function index($name, $pass)
     {
-        $result = '<html><body>';
-        $result .= '<h1>Subscribed Services</h1>';
-        $result .= '<ol>';
-        $arr = $this->getSubscribedServices();
-        foreach ($arr as $key => $value) {
-            $result .= '<li>' . $key . '</li>';
-        }
-        $result.= '</ol>';
+        $result = '<html><body><ol>';
+        $result .= '<h1>Parameter</h1>';
+        $result .= '<p>name: ' . $name . '</p>';
+        $result .= '<p>pass: ' . $pass . '</p>';
         $result .= '</body></html>';
         return new Response($result);
     }
