4cceb4486960f46055765ed75c8cb57f392916fa
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-04 15:00:47 +0900

    ✨ /hello にアクセスすると「Hello Symfony!」表示される

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,17 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController extends AbstractController
+class HelloController
 {
-    #[Route('/hello', name: 'app_hello')]
-    public function index(): Response
+    public function index()
     {
-        return $this->render('hello/index.html.twig', [
-            'controller_name' => 'HelloController',
-        ]);
+        return new Response('Hello Symfony!');
     }
 }
