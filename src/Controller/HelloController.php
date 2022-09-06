629d63d0ef26a30a5125dcc529ef0b494119915f
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-07 08:10:36 +0900

    ✨ エラーの送信

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,29 +7,54 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-
 class HelloController extends AbstractController
 {
     #[Route('/hello', name:'hello')]
     public function index(Request $request)
     {
-        $encoders = array(new XmlEncoder());
-        $normalizers = array(new ObjectNormalizer());
-        $serializer = new Serializer($normalizers, $encoders);
-
-
-        $data = array(
-            'name'=>array('first'=>'Hanako','second'=>'Tanaka'),
-            'age'=>29, 'mail'=>'hanako@flower.san'
+        $content = <<< EOM
+        <html><head><title>Hello</title></head>
+        <body><h1>Hello!</h1>
+        <p>this is Symfony sample page.</p>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
         );
+        return $response;
+    }
 
-        $response = new Response();
-        $response->headers->set('Content-Type', 'xml');
-        $result = $serializer->serialize($data, 'xml');
-        $response->setContent($result);
+    #[Route('/notfound', name:'notfound')]
+    public function notfound(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>ERROR</title></head>
+        <body><h1>ERROR! 404</h1>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_NOT_FOUND,
+            array('content-type' => 'text/html')
+        );
+        return $response;
+    }
+
+    #[Route('/error', name:'error')]
+    public function error(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>ERROR</title></head>
+        <body><h1>ERROR! 500</h1>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_INTERNAL_SERVER_ERROR,
+            array('content-type' => 'text/html')
+        );
         return $response;
     }
 }
