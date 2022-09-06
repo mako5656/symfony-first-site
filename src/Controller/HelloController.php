9426a188954290391a6d4f59bc8823f9e8cff428
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-06 23:13:32 +0900

    ✨ 配列をXMLデータに変換し、ResponseでXMLとして送信する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,19 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello', name:'hello')]
     public function index(Request $request)
     {
+        $encoders = array(new XmlEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+
         $data = array(
-            'name'=>array('first'=>'Taro','second'=>'Yamada'),
-            'age'=>36, 'mail'=>'taro@yamada.kun'
+            'name'=>array('first'=>'Hanako','second'=>'Tanaka'),
+            'age'=>29, 'mail'=>'hanako@flower.san'
         );
-        return new JsonResponse($data);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'xml');
+        $result = $serializer->serialize($data, 'xml');
+        $response->setContent($result);
+        return $response;
     }
 }
