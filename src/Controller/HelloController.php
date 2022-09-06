0824b32487ccd7271f955ec84458f72f44dde8dc
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-06 23:02:34 +0900

    ✨ JsonResponseを利用してJSONデータを出力

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -13,20 +14,10 @@ class HelloController extends AbstractController
     #[Route('/hello', name:'hello')]
     public function index(Request $request)
     {
-        $result = '<html><body><ol>';
-        $result .= '<h1>Parameter</h1>';
-        $result .= '<p>This is index page.</p>';
-        $result .= '</body></html>';
-        return new Response($result);
-    }
-
-    #[Route('/other/{domain}', name:'other')]
-    public function other(Request $request, $domain='')
-    {
-        if ($domain == ''){
-            return $this->redirect('/hello');
-        } else {
-            return new RedirectResponse("http://{$domain}.com");
-        }
+        $data = array(
+            'name'=>array('first'=>'Taro','second'=>'Yamada'),
+            'age'=>36, 'mail'=>'taro@yamada.kun'
+        );
+        return new JsonResponse($data);
     }
 }
