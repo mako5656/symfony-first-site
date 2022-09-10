7c1a1e30c2d9fe8e65d6a475bef3d758c1c04695
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-10 12:16:54 +0900

    ♻️ GETおよびPOSTを同一アクションで処理

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -14,22 +14,12 @@ class HelloController extends AbstractController
     #[Route('/hello', name:'hello')]
     public function index(Request $request)
     {
-        return $this->render('hello/index.html.twig', [
-            'title' => 'Hello',
-            'message' => 'あなたのお名前：',
-        ]);
-    }
-
-    #[Route('/other', name:'other')]
-    public function other(Request $request)
-    {
-        /**
-         * $request->request がPOST送信された値を扱うオブジェクト
-         * requestにせっていされたオブジェクトのgetメソッドを使って送信フォームの値を取り出す
-         * 引数には、フォームのname属性を指定
-        */
-        $input = $request->request->get('input');
-        $msg = 'こんにちは、' . $input . 'さん！';
+        if ($request->getMethod() == 'POST'){
+            $input = $request->request->get('input');
+            $msg = 'こんにちは、' . $input . 'さん！';
+        } else {
+            $msg = 'お名前は？';
+        }
         return $this->render('hello/index.html.twig', [
             'title' => 'Hello',
             'message' => $msg,
