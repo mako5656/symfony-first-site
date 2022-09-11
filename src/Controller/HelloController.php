0f371c9715a7df3b2d0a3851c717820e755d68a4
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-11 13:34:11 +0900

    ✨Twigテンプレートを使ってオリジナルのページ作成

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,52 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello', name:'hello')]
-    public function index(Request $request, SessionInterface $session)
+    public function index(Request $request)
     {
-        $data = new MyData();
-        $form = $this->createFormBuilder($data)
-            ->add('data', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Click'])
-            ->getForm();
-
-
-        if ($request->getMethod() == 'POST'){
-            $form->handleRequest($request);
-            $data = $form->getData();
-            if ($data->getData() == '!'){
-                $session->remove('data');
-            } else {
-                $session->set('data',$data->getData());
-            }
-        }
-
-
         return $this->render('hello/index.html.twig', [
-            'title' => 'Hello',
-            'data' => $session->get('data'),
-            'form' => $form->createView(),
-        ]);
-    }
-}
-
-// データ用クラス
-class MyData
-{
-    protected $data = '';
-
-    public function getData()
-    {
-        return $this->data;
-    }
-    public function setData($data)
-    {
-        $this->data = $data;
+           'title' => 'Hello',
+           'message' => 'これはサンプルのテンプレート画面です。',
+       ]);
     }
 }
