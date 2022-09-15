da1fe0622b9b578b0f7848d036f5dcc70af81ff8
Makoto Ikami <makoto.ikami@gmail.com>
2022-09-15 19:03:04 +0900

    ✨ エンティティの一覧を表示する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +13,9 @@ class HelloController extends AbstractController
     #[Route('/hello', name:'hello')]
     public function index(Request $request)
     {
-        $data = [
-            array('name'=>'Taro','age'=>37,'mail'=>'taro@yamada'),
-            array('name'=>'Hanako','age'=>29,'mail'=>'hanako@flowe'),
-            array('name'=>'Sachiko','age'=>43,'mail'=>'sachico@happy'),
-            array('name'=>'Jiro','age'=>18,'mail'=>'jiro@change'),
-        ];
+        $repository = $this->getDoctrine()
+            ->getRepository(Person::class);
+        $data = $repository->findall();
         return $this->render('hello/index.html.twig', [
             'title' => 'Hello',
             'data' => $data,
