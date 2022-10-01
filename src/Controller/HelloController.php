a2ffc027bb966827181f6b829c5c72bb883dda0e
Makoto Ikami <makoto.ikami@gmail.com>
2022-10-01 17:21:24 +0900

    「src」のファイルから、アノテーションを使っているものを検索する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -31,7 +31,7 @@ class HelloController extends AbstractController
     public function index(Request $request)
     {
         $finder = new Finder();
-        $finder->files()->depth('<=0')->notName('*.lock')->in('../');
+        $finder->files()->depth('<=1')->contains('* @')->in('../src/');
 
         return $this->render('hello/index.html.twig', [
             'title' => 'Hello',
